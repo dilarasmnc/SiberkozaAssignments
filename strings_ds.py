1dd028a9d801804442674967ecb3efa39e90bdb3
dilarasmnc <124294650+dilarasmnc@users.noreply.github.com>
2023-02-03 19:10:00 +0300

    Update strings_ds.py

--- strings_ds.py
+++ strings_ds.py
@@ -1,6 +1,15 @@
-# Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
+name_ds = 'Dilara'
+age_ds = 22
+
+# Concatenate
+print('Hello, my name is ' + name_ds+ ' and I am ' + str(age_ds))
 
 
 # String Formatting
+
+# Arguments by position
+print('My name is {name}' and I am {age}'.format(name=name_ds, age=age_ds))
+
+#F-Strings
 
 # String Methods
