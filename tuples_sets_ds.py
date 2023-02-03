abc128c866044373a7813815c2d1c4c18b19f416
dilarasmnc <124294650+dilarasmnc@users.noreply.github.com>
2023-02-03 22:04:49 +0300

    Update tuples_sets_ds.py

--- tuples_sets_ds.py
+++ tuples_sets_ds.py
@@ -1,1 +1,52 @@
 
+# Create tuple
+fruits_ds = ('Apples', 'Oranges', 'Grapes')
+# fruits2_ds = tumple(('Apples', 'Oranges', 'Grapes')) 
+fruits2_ds = ('Apples',)
+
+print(fruits2_ds, type(fruits2_ds))
+
+# Get value
+
+print(fruits_ds[1])
+
+# Can't change value
+
+fruits_ds[0] = 'Pears'
+
+# Delete tuple
+del fruits2_ds 
+
+print(fruits2_ds) 
+
+# Get lenght
+print(len(fruits_ds)
+      
+# Create set
+fruits_set_ds = {'Apples', 'Oranges', 'Mango'}
+
+# Check if in set
+print('Apples' in fruits_set_ds)
+
+# Add to set
+fruits_set_ds.add('Grape')
+ 
+print(fruits_set_ds
+
+# Remove from set
+fruits_set_ds.remove('Grape')     
+
+print(fruits_set_ds)
+
+# Clear set      
+fruits_set_ds.clear()     
+      
+print(fruits_set_ds)      
+
+# Delete
+del fruits_set_ds
+      
+print(fruits_set_ds)      
+      
+      
+      
