8548b02f6660089122f8a0588e9462b992cee80e
dilarasmnc <124294650+dilarasmnc@users.noreply.github.com>
2023-02-04 21:02:59 +0300

    Update modules_ds.py

--- modules_ds.py
+++ modules_ds.py
@@ -1,3 +1,46 @@
-# module
+# Core modules
 
-import datetime_ds
+# import
+import datetime
+
+today_ds = datetime.date.today()
+
+print(today_ds)
+
+# from
+import datetime
+from datetime import date
+
+today_ds = date.today()
+
+print(today_ds)
+
+#
+import datetime
+from datetime import date
+import time
+
+today = date.today()
+timestamp_ds = time.time()
+
+print(timestamp_ds)
+
+#
+import datetime
+from datetime import date
+import time
+from time import time
+
+today = date.today()
+timestamp_ds = time()
+
+print(timestamp_ds)
+
+# Pip module
+
+# pip (pip3 install camelcase)(global)
+
+from camelcase import CamelCase
+
+c_ds = CamelCase()
+print(c_ds.hump('hello there world'))
