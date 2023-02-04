65f92524f053d4368e5b39992704fc61f74054ab
dilarasmnc <124294650+dilarasmnc@users.noreply.github.com>
2023-02-04 23:11:36 +0300

    Update py_json_ds.py

--- py_json_ds.py
+++ py_json_ds.py
@@ -2,10 +2,19 @@
 import json
 
 # Sample JSON
+user_dsJSON ='{"first_name_ds": "Dilara", "last_name_ds": "Samancı", "age_ds": 30}'
 
+# Parse to dict
+user_ds = json.loads(user_dsJSON)
 
+print(user_ds)
+print(user_ds['first_name_ds'])
 
+car_ds = {'make_ds': 'Ford', 'model_ds': 'Mustang', 'year_ds': 1970}
 
+car_dsJSON = json.dumps(car_ds)
+
+print(car_dsJSON)
 
 
 
