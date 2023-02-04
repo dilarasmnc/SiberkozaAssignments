6a96b1685336d939be8df2b0a388c675294c8ae7
dilarasmnc <124294650+dilarasmnc@users.noreply.github.com>
2023-02-04 19:36:52 +0300

    Update functions_ds.py

--- functions_ds.py
+++ functions_ds.py
@@ -1,6 +1,34 @@
 # Create function
-def sayHello(name ='Sam'):
-  print(f'Hello {name}')
+def sayHello(name_ds):
+  print(f'Hello {name_ds}')
   
   
+sayHello('Dilara Samancı')
+
+# Create function
+def  sayHello(name_ds = 'Dilara'):
+  print(f'Hello {name_ds}')
+
 sayHello()
+
+# Return values
+def getSum(num1_ds, num2_ds):
+  total = num1_ds + num2_ds 
+  return total
+
+print(getSum(3, 4))
+
+# Return values
+def getSum(num1_ds, num2_ds):
+  total = num1_ds + num2_ds 
+  return total
+
+num_ds = getSum(3, 4)
+
+print(num_ds)
+
+# Lambda function 
+
+getSum = lambda num1_ds, num2_ds : num1_ds + num2_ds
+
+print(getSum(10, 3))
