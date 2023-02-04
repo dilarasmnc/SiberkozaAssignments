90e81ff685ef17a4fccb2087d7f56ff13b5d9ab5
dilarasmnc <124294650+dilarasmnc@users.noreply.github.com>
2023-02-04 20:05:57 +0300

    Update conditionals_ds.py

--- conditionals_ds.py
+++ conditionals_ds.py
@@ -1,6 +1,6 @@
 
 x_ds = 10
-y_ds = 5
+y_ds = 50
 
 # Comparison Operators (==, !=, >, <, >=, <=) 
 
@@ -14,15 +14,52 @@ if x_ds > y_ds:
 else:
   print(f'{y_ds} is greather than {x_ds}')
 
+# elif
+if x_ds > y_ds:
+  print(f'{x_ds} is greather than {y_ds}')
+elif x_ds == y_ds:
+  print(f'{x_ds} is equal to {y_ds}')
+else:
+  print(f'{y_ds} is greather than {x_ds}')
 
+# Nested if
+if  x_ds > 2:
+  if  x_ds <= 10:
+    print(f'{x_ds} is greather than 2 and less than or equal to 10')
+  
 # Logical operators (and, or, not) 
 
+# and
+if  x_ds > 2 and x <= 10:
+     print(f'{x_ds} is greather than 2 and less than or equal to 10')
+# or
+if  x_ds > 2 or x <= 10:
+     print(f'{x_ds} is greather than 2 or less than or equal to 10')
 
-
+# not
+if not(x_ds == y_ds):
+  print(f'{x_ds} is not equal to {y_ds}')
 
 # Membership Operators (in, not in) 
 
+numbers_ds = [1,2,3,4,5]
 
+# in
+if x_ds in numbers_ds:
+  print(x_ds in numbers_ds)
 
+# not in 
+if x_ds not in numbers_ds:
+  print(x_ds not in numbers_ds)
 
 # Identity Operators (is, is not) 
+
+# is 
+if x_ds is y_ds:
+  print(x_ds is y_ds)
+
+# is not 
+if x_ds is not y_ds:
+  print(x_ds is not y_ds)
+  
+  
