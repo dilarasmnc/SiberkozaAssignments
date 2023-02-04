10c2d74b1074871998ac5f1a3889692be1b5e500
dilarasmnc <124294650+dilarasmnc@users.noreply.github.com>
2023-02-04 22:54:29 +0300

    Update files_ds.py

--- files_ds.py
+++ files_ds.py
@@ -4,3 +4,22 @@
 myFile_ds = open('myfile_ds.txt', 'w')
 
 # Get some info
+print('Name: ', myFile_ds.name)
+print('Is Closed: ', myFile_ds.closed)
+print('Opening Mode: ', myFile_ds.mode)
+
+# Write to file
+myFile_ds.write('I Love Python')
+myFile_ds.write(' and Java')
+myFile.close()
+
+# Append to file
+myFile_ds = open('myfile_ds.txt', 'a')
+myFile_ds.write(' I also like PHP')
+myFile.close()
+
+# Read from file
+myFile_ds = open('myfile_ds.txt', 'r+')
+text_ds = myFile_ds.read(100)
+
+print(text_ds)
