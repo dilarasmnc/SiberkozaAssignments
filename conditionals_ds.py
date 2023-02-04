7c13ad3fc87586aa38fb30b47d27cfbdded7a886
dilarasmnc <124294650+dilarasmnc@users.noreply.github.com>
2023-02-04 19:43:47 +0300

    Update conditionals_ds.py

--- conditionals_ds.py
+++ conditionals_ds.py
@@ -1,19 +1,28 @@
-# If/ Else conditions are used to decide to do something based on something being true or false
+
+x_ds = 10
+y_ds = 5
+
+# Comparison Operators (==, !=, >, <, >=, <=) 
+
+# Simple if
+if x_ds > y_ds:
+  print(f'{x_ds} is greather than {y_ds}')
+
+# If/ Else conditions 
+if x_ds > y_ds:
+  print(f'{x_ds} is greather than {y_ds}')
+else:
+  print(f'{y_ds} is greather than {x_ds}')
+
+
+# Logical operators (and, or, not) 
 
 
 
-# Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
 
-
-
-# Logical operators (and, or, not) - Used to combine conditional statements
-
-
-
-
-# Membership Operators (in, not in) - Membership operators are used to test if a sequence is presented in an object
+# Membership Operators (in, not in) 
 
 
 
 
-# Identity Operators (is, is not) - Compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+# Identity Operators (is, is not) 
